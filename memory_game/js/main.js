1f5b1e5e828e741cbdf7a284808d3812725d4c5a
Holly Klose <hollyklose@gmail.com>
2019-04-23 11:22:52 -0400

    created checkForMatch and flipCard functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
+var checkForMatch = function() {
+		// Need to add if cardsinPlay.length is 2, then:
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
-	}
+		console.log("Sorry, try again.");
+	};
 };
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
+
+
